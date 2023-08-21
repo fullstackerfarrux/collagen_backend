7b7ab3b0ac03c57a271526a5d902b2e2da55483f
farrux <orziyevfarrux01@gmail.com>
2023-08-22 01:16:01 +0500

    send to group changed

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ bot.on("location", async (msg) => {
         `;
 
   await axios.post(
-    `https://api.telegram.org/bot${token}/sendMessage?chat_id=-1001918190466&parse_mode=html=${message}`
+    `https://api.telegram.org/bot${token}/sendMessage?chat_id=-1001918190466&parse_mode=markdown&text=${message}`
   );
   await axios.post(
     `https://api.telegram.org/bot${token}/sendLocation?chat_id=${chat_id}&latitude=${latitude}&longitude=${longitude}`
